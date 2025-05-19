eb3de7c013ddfa2d1407242f2d297f8110d8b628
Kin Leon Zinzombe <kinzinzombe07@gmail.com>
2025-05-19 10:08:20 +0200

    feat: add authentication layout and page components
    - Introduced `AuthLayout` and `AuthPage` for user authentication.
    - Updated `layout.tsx` to include `QueryClientProviderWrapper`.
    - Added various UI components including `Alert`, `Button`, `Calendar`, `Card`, `Dialog`, `Input`, `Label`, `Separator`, `Sheet`, `Sonner`, and `Textarea` using Radix UI and custom styles.
    - Enhanced `package.json` with new dependencies for UI components and date handling.

---
+++ src/app/auth/page.tsx
@@ -0,0 +1,16 @@
+function AuthPage() {
+  return (
+    <section className="min-h-screen w-full flex flex-col justify-center items-center">
+      <article>
+        <h2 className="text-2xl font-semibold capitalize">Welcome back</h2>
+
+        {/* Email label and input */}
+        <article className="flex flex-col gap-1">
+            
+        </article>
+      </article>
+    </section>
+  );
+}
+
+export default AuthPage;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import { Geist, Geist_Mono, Poppins } from 'next/font/google';
 import './globals.css';
+import QueryClientProviderWrapper from '@/providers/QueryClientProviderWrapper';
 
 const poppins = Poppins({
   subsets: ['latin'],
@@ -55,7 +56,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className={`${poppins.variable}`}>
-      <body className={`${poppins.variable} antialiased`}>{children}</body>
+      <body className={`${poppins.variable} antialiased`}>
+        <QueryClientProviderWrapper>{children}</QueryClientProviderWrapper>
+      </body>
     </html>
   );
 }
