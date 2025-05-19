3dde31f01fbc2827fa57e82d54221749a25b754f
Kin Leon Zinzombe <kinzinzombe07@gmail.com>
2025-05-19 11:10:21 +0200

    feat: add Avatar component using Radix UI and integrate into Sidebar

--- src/components/shared/Sidebar.tsx
+++ src/components/shared/Sidebar.tsx
@@ -5,8 +5,58 @@ import {
   SidebarGroup,
   SidebarHeader,
 } from '@/components/ui/sidebar';
+import { authClient } from '@/lib/auth-client';
+import { BanknoteArrowUp, BotMessageSquare, ChartLine, DoorOpen, Settings2 } from 'lucide-react';
+import { Avatar, AvatarImage, AvatarFallback } from '../ui/avatar';
 
 function AppSideBar() {
+  // Get the user's data
+  const { data, isPending } = authClient.useSession();
+
+  const primaryLinks = [
+    {
+      name: 'Dashboard',
+      slug: 'dashboard',
+      icon: <ChartLine />,
+    },
+    {
+      name: 'Bots',
+      slug: 'bots',
+      icon: <BotMessageSquare />,
+    },
+  ];
+
+  const secondaryLinks = [
+    {
+      name: 'Settings',
+      slug: 'settings',
+      icon: <Settings2 />,
+    },
+    {
+      name: 'Profile',
+      slug: 'profile',
+      icon: (
+        <Avatar>
+          <AvatarImage src={data?.user?.image || ''} alt={`${data?.user?.email}'s avatar`} />
+          <AvatarFallback>{data?.user?.email.charAt(0)}</AvatarFallback>
+        </Avatar>
+      ),
+    },
+  ];
+
+  const footerLinks = [
+    {
+      name: 'Logout',
+      slug: 'logout',
+      icon: <DoorOpen />,
+    },
+    {
+      name: 'Billing',
+      slug: 'billing',
+      icon: <BanknoteArrowUp />,
+    },
+  ];
+
   return (
     <Sidebar>
       <SidebarHeader>
