98db88e41447dafd176418c00dc02c4d370fae92
Kin Leon Zinzombe <kinzinzombe07@gmail.com>
2025-05-19 12:55:51 +0200

    feat: implement user data fetching in DashboardPage and add API route for user data

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,5 +1,25 @@
+'use client';
+import { useQuery } from '@tanstack/react-query';
+import axios from 'axios';
+
 function DashboardPage() {
-  return <div></div>;
+  // Fetch the user's data
+  const { data, isLoading: gettingUserData } = useQuery({
+    queryKey: ['data'],
+    queryFn: async () => {
+      const res = await axios.get('/api/data');
+
+      return res.data.data;
+    },
+  });
+
+  console.log('Data: ', data);
+
+  return (
+    <section>
+      <h1>Dashboard</h1>
+    </section>
+  );
 }
 
 export default DashboardPage;
