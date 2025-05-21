4423f38a990e4a54400b137d117255b489d4142f
Kin Leon Zinzombe <kinzinzombe07@gmail.com>
2025-05-21 21:44:56 +0200

    [ADD]: Forced nodejs runtime instead of edge

--- web/src/middleware.ts
+++ web/src/middleware.ts
@@ -12,5 +12,6 @@ export async function middleware(request: NextRequest) {
 }
 
 export const config = {
+  runtime: "nodejs",
   matcher: ['/((?!^$|auth|_next/static|_next/image|favicon.ico).*)'],
 };
