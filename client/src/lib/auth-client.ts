2a194ac093d17c66365d9cc862f9789a05df1f83
Kin Leon Zinzombe <kinzinzombe07@gmail.com>
2025-05-29 13:44:27 +0200

    [ADD]: Prod and dev server urls to client

--- client/src/lib/auth-client.ts
+++ client/src/lib/auth-client.ts
@@ -1,6 +1,9 @@
 import {magicLinkClient} from "better-auth/client/plugins";
 import {createAuthClient} from "better-auth/react";
 export const authClient = createAuthClient({
-  baseURL: import.meta.env.BETTER_AUTH_URL as string,
+  baseURL:
+    import.meta.env.MODE === "production"
+      ? "https://api.botworld.pro"
+      : "http://localhost:8080",
   plugins: [magicLinkClient()],
 });
