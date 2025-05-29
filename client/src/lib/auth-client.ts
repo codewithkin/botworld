7542f2d0e0f759fe2fac67cd87ec2d13177c0a21
Kin Leon Zinzombe <kinzinzombe07@gmail.com>
2025-05-29 13:39:10 +0200

    [ADD]: server base url to auth client

--- client/src/lib/auth-client.ts
+++ client/src/lib/auth-client.ts
@@ -1,5 +1,6 @@
-import { magicLinkClient } from "better-auth/client/plugins";
-import { createAuthClient } from "better-auth/react";
+import {magicLinkClient} from "better-auth/client/plugins";
+import {createAuthClient} from "better-auth/react";
 export const authClient = createAuthClient({
+  baseURL: import.meta.env.BETTER_AUTH_URL as string,
   plugins: [magicLinkClient()],
 });
